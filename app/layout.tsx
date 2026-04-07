de7ece611c7185d26095ce12cbbaffbfedec3b5f
Rahul Reddy <rahuldean@gmail.com>
2026-04-06 21:45:50 -0700

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from './header'
 import { Footer } from './footer'
 import { NAME } from './data'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from "@vercel/analytics/next"
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -58,6 +59,7 @@ export default function RootLayout({
             </div>
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
